3f52f758cc8e545ead9120bfedeb15093e10b6a3
Javier D Perez <javier.d.perez@assurant.com>
2020-02-28 14:05:41 -0500

    Update presentation and add SessionGroupDto

---
+++ ConferenceTracker.Model/Dtos/SessionGroupDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConferenceTracker.Model.Dtos
+{
+    public class SessionGroupDto
+    {
+        public DateTime Date { get; set; }
+        public List<Session> Sessions { get; set; }
+    }
+}
